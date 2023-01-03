85d95fb9b9eaaa5b5753bbd7f22628ce801dd1b2
elliesea <la.belle.mer.517@gmail.com>
2023-01-03 23:04:43 +0900

    モデルのリレーションの実装

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -13,16 +13,28 @@
         <form action="/posts" method="POST">
             @csrf
             <div class ="title">
-                <h2>Tilte</h2>
+                <h2>Title</h2>
                 <input type ="text" name="post[title]" placeholder="タイトル" value="{{ old('post.title') }}"/>
                 <p class = "title__error" style="color:red">{{ $errors->first('post.title') }}</p>
             </div>
+            
             <div class="body">
                 <h2>Body</h2>
                 <textarea name="post[body]" placeholder="今日も1日お疲れました。">{{ old('post.body') }}</textarea>
                 <p class="body__error" style="color:red">{{ $errors->first('post.body') }}</p>
             </div>
+            
+            <div class="category">
+                <h2>Category</h2>
+                <select name="post[category_id]">
+                @foreach($categories as $category)
+                    <option value="{{ $category->id }}">{{ $category->name }}</option>
+                @endforeach
+                </select>
+            </div>
+        
             <input type="submit" volume="保存"/>
+            
         </form>
         <div class="back">[<a href="/">back</a>]</div>
     </body>
